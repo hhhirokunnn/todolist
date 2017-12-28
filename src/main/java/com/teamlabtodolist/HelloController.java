25254a0ef1a3d8c2932e8815482e41ef3fa86103
mukaihiroto <mukaihiroto@teawm-lab.com>
2017-12-28 17:49:33 +0900

    first commit

---
+++ src/main/java/com/teamlabtodolist/HelloController.java
@@ -0,0 +1,18 @@
+package com.teamlabtodolist;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+}
---
+++ src/main/java/com/teamlabtodolist/TeamlabTodolistApplication.java
@@ -0,0 +1,12 @@
+package com.teamlabtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamlabTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamlabTodolistApplication.class, args);
+	}
+}
