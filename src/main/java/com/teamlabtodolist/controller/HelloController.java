aac40cbc9b6357743299291215767d5e27160cd7
mukaihiroto <mukaihiroto@teawm-lab.com>
2017-12-29 14:50:30 +0900

    #11 view サーバーとフロントの連携

--- src/main/java/com/teamlabtodolist/controller/HelloController.java
+++ src/main/java/com/teamlabtodolist/controller/HelloController.java
@@ -1,29 +1,22 @@
 package com.teamlabtodolist.controller;
 
-import java.util.List;
-
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.teamlabtodolist.entity.Sample;
 import com.teamlabtodolist.model.SampleModel;
 
 @Controller
-@EnableAutoConfiguration
 public class HelloController {
 
 	@Autowired
     private SampleModel sampleModel;
 	
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return sampleModel.findAll().get(0).getName();
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    String index(Model model) {
+		model.addAttribute("data",sampleModel.findAll().get(0).getName());
+        return "index";
     }
 }
