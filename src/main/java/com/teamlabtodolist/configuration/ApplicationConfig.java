012c265af9e1493cd19406b134afc8a72191edb4
mukaihiroto <mukaihiroto@teawm-lab.com>
2018-01-22 14:31:21 +0900

    コメント追加
    コンフィグクラス

--- src/main/java/com/teamlabtodolist/configuration/ApplicationConfig.java
+++ src/main/java/com/teamlabtodolist/configuration/ApplicationConfig.java
@@ -7,9 +7,18 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 設定用のクラス
+ * @author mukaihiroto
+ *
+ */
 @Configuration
 public class ApplicationConfig {
 
+	/**
+	 * SQLのログを見るためのメソッド
+	 * @return
+	 */
     @Bean
     @ConfigurationProperties("spring.datasource")
     public DataSource dataSource() {
