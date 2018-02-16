103a80c015c04675a975965b46c677ce86588ec5
mukaihiroto <mukaihiroto@teawm-lab.com>
2018-02-16 18:52:03 +0900

    googleapiを使う

--- src/main/resources/static/js/oauth.js
+++ src/main/resources/static/js/oauth.js
@@ -11,15 +11,10 @@ var logout = function() {
     return true;
 }
 $.ajaxSetup({
-    beforeSend : function(xhr, settings) {
-      if (settings.type == 'POST' || settings.type == 'PUT'
-          || settings.type == 'DELETE') {
-        if (!(/^http:.*/.test(settings.url) || /^https:.*/
-            .test(settings.url))) {
-          // Only send the token to relative URLs i.e. locally.
+    beforeSend : function(xhr, req) {
+      if (req.type == 'POST') {
           xhr.setRequestHeader("X-XSRF-TOKEN",
               Cookies.get('XSRF-TOKEN'));
-        }
       }
     }
 });
